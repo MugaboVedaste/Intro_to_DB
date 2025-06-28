700368629bbc9045ca353bd7be12354abb1ae0ce
Vedaste MUGABO <vedasteapp@gmail.com>
2025-06-28 18:24:22 +0200

    Working on Database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -21,7 +21,7 @@ CREATE TABLE Orders (
 );
 CREATE TABLE Order_Details (
   order_details_id INT PRIMARY KEY
-  , FOREIGN KEY order_id REFERENCES Orders(order_id)
+  , FOREIGN KEY (order_id) REFERENCES Orders(order_id)
   , FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
   , FOREIGN KEY (book_id) REFERENCES Books(book_id)
   , quantity DOUBLE
